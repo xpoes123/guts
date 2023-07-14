e7fb25aee5e9ceea3b8c4b599248e9dfb9b4307e
David Jiang <logiccubing@gmail.com>
2023-07-14 01:15:53 -0400

    Update guts_deck.py

--- guts_deck.py
+++ guts_deck.py
@@ -1,3 +1,5 @@
+# This program deals primarily with the deck itself and computing which hand is better
+
 import random
 from constants import *
 
@@ -6,15 +8,17 @@ class Deck:
         self.cards = []
         self.build()
 
+    # Generates the deck that we are working with
     def build(self):
         for value in RANKS:
             for suit in SUITS:
                 self.cards.append((value, suit))
   
+    #Shuffles the deck lol
     def shuffle(self):
-        self.build()
         random.shuffle(self.cards)
         
+    # Deals out a singular card
     def deal(self):
         if len(self.cards) > 1:
             return self.cards.pop()
@@ -24,9 +28,11 @@ class Hand(Deck):
         self.cards = []
         self.card_img = []
 
+    # Adds a card to our cards array so our hand is defined
     def add_card(self, card):
         self.cards.append(card)
 
+    # Returns the names of the card so it can be used to display in pygame
     def display_cards(self):
         for card in self.cards:
             cards = "".join((card[0], card[1]))
@@ -36,21 +42,22 @@ class Hand(Deck):
     # compares two hands between self and hand2
     # Returns 1 if self wins and -1 if opponent wins and 0 if it is a tie
     def comp_hand(self, hand2):
-        print(self.cards[0], self.cards[1])
-        print(hand2.cards[0], hand2.cards[1])
-
         # Checks if the hands are paired or not
         selfPair = False
         if self.cards[0][0] == self.cards[1][0]:
             selfPair = True
+
         opPair = False
         if hand2.cards[0][0] == hand2.cards[1][0]:
             opPair = True
+        
+        # Checks if one hand is better based on if a hand is paired or not
         if opPair and not selfPair:
             return -1
         elif selfPair and not opPair:
             return 1
         elif opPair and selfPair:
+            # Checks the hierchy of cards given that they are paired
             if RANKS.index(self.cards[0][0]) > RANKS.index(hand2.cards[0][0]):
                 return 1
             elif RANKS.index(self.cards[0][0]) == RANKS.index(hand2.cards[0][0]):
@@ -63,6 +70,7 @@ class Hand(Deck):
             selfMin = min(RANKS.index(self.cards[0][0]), RANKS.index(self.cards[1][0]))
             opMax =  max(RANKS.index(hand2.cards[0][0]), RANKS.index(hand2.cards[1][0]))
             opMin =  min(RANKS.index(hand2.cards[0][0]), RANKS.index(hand2.cards[1][0]))
+            
             if(selfMax > opMax):
                 return 1
             elif(selfMax < opMax):
